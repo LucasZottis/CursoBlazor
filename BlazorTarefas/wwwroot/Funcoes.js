f0330ac8ba33f783d5d14266bc7c90a24e4c07d9
Lucas Zottis <zottis.lucas@hotmail.com>
2022-07-05 05:55:58 -0300

    Aulas de chamada de métodos estáticos e de instância do JavaScript pelo C# e vice versa.

---
+++ BlazorTarefas/wwwroot/Funcoes.js
@@ -0,0 +1,15 @@
+﻿window.MostraAlerta = (message) => {
+    alert(message);
+}
+
+function getTotalTarefas() {
+    DotNet.invokeMethodAsync("BlazorTarefas", "ObterTotalTarefas").then(resultado => {
+        alert("Total de tarefas: " + resultado);
+    });
+}
+
+function getTotalTarefasInstancia(dotnet) {
+    dotnet.invokeMethodAsync("ObterTotalTarefasInstancia").then(resultado => {
+        alert("Total de tarefas: " + resultado);
+    });
+}

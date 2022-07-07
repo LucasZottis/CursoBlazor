f5acfe427b031bc515549cb33bb6a47beafe15df
Lucas Zottis <zottis.lucas@hotmail.com>
2022-07-06 21:25:37 -0300

    Aula sobre parâmetros em cascata

---
+++ ParametrosCascata/Utils/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace ParametrosCascata.Utils
+{
+    public class Config
+    {
+        public string CorFundo { get; set; } = "Yellow";
+
+        public string TamanhoFonte { get; set; } = "4em";
+    }
+}
